fe79403f78e85ab11c8c7566a805121dd5c1a8ab
QBpopo <2461318613@qq.com>
2026-08-15 21:48:26 +0800

    dependencies configuration

---
+++ oxlint.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+	plugins: ["typescript", "unicorn", "oxc"],
+	categories: {
+		correctness: "error",
+	},
+	env: { builtin: true },
+});
