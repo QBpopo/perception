811e752ca39a9f9199538a4e922ce5c923b9ae49
QBpopo <2461318613@qq.com>
2026-08-20 11:17:15 +0800

    github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 
+const [_repositoryOwner, repositoryName] = process.env.GITHUB_REPOSITORY?.split("/") ?? [];
+const base = process.env.GITHUB_ACTIONS ? `/${repositoryName ?? ""}/` : "/";
+
 export default defineConfig({
+	base,
 	resolve: {
 		tsconfigPaths: true,
 	},
